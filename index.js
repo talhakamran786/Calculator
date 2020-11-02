c8babd85d6fa5db611532c56800dc8fbc2d52fa2
talhakamran786 <talhakamrantravalmakkah@gmail.com>
2020-11-02 09:49:50 -0800

    Many Changes in All Files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+ 
+
 
  function ADD(){
     var Data1 = document.getElementById("type").value;
@@ -57,20 +59,35 @@ function Calculate(){
   }
 }
   function dlt(){
-      var mydata2 = document.getElementById("type2").value;
-      console.log(mydata2)
-      
-    //    = '';
-      
-      console.log(mydata2[(mydata2.length-1)])
+   var d = document.getElementById('type2').value;
 
-      
+   var b = d.split("");
 
-      
+   b.pop();
+
+   var c ="";
+
+   for( i=0; i<b.length; i++ ){
+c+=b[i]
+}
+
+document.getElementById("type2").value = c;
+
+  }
   
   
-    } 
-  
+// var MyData = prompt("Dataaa");
 
+// var b = MyData.split("");
+
+// b.pop();
+
+// var c = "";
+
+// for( i=0; i<b.length; i++ ){
+//   c += b[i]
+  
+// }
+// alert(c)
 
 
